c898ac85c8b10e5ab9d6ac2fb7941fa5e5cbdafa
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-02 09:55:50 +0200

    Rename entry point to runGitStats

--- src/nativeMain/kotlin/NativeMain.kt
+++ src/nativeMain/kotlin/NativeMain.kt
@@ -1,8 +1,8 @@
-import cli.runGitStandup
+import cli.runGitStats
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     runBlocking {
-        runGitStandup(args)
+        runGitStats(args)
     }
 }
