595e87b0e10dafd48cea0f38d47ce14216a77c2e
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-02 09:56:03 +0200

    Do one call to git-log instad of n+1

--- src/commonMain/kotlin/cli/GitStandup.kt
+++ src/commonMain/kotlin/cli/GitStandup.kt
@@ -10,7 +10,7 @@ var options = ExecuteCommandOptions(directory = ".", abortOnError = true, redire
 /***
  * CUSTOMIZE_ME: this file is all specific to git-standup and can be deleted once understood
  */
-suspend fun runGitStandup(args: Array<String>) {
+suspend fun runGitStats(args: Array<String>) {
 
     val jsPackage = "/build/js/packages/kotlin-cli-starter"
     val pwd = executeCommandAndCaptureOutput(listOf("pwd"), options)
@@ -38,22 +38,52 @@ fun printStats(fileStats: Map<String, Int>) {
 }
 
 suspend fun parseCommits(): Map<String, Int> {
-    val commits = executeCommandAndCaptureOutput("git log --pretty=format:%h".split(" "), options)
-    val commitsList = commits.lines()
+    val gitLogOutput: String = executeCommandAndCaptureOutput("git log  --numstat --name-only".split(" "), options)
+    val  commits = parseGitlogOutput(gitLogOutput)
 
     val fileStats = mutableMapOf<String, Int>()
-    commitsList.forEach {
-        val commits = executeCommandAndCaptureOutput("git diff-tree --no-commit-id --name-only -r $it".split(" "), options)
-        parseCommitFiles(commits, fileStats)
+    commits.forEach { commit ->
+        parseCommitFiles(commit, fileStats)
     }
 
     return fileStats
 }
 
-suspend fun parseCommitFiles(commits: String, fileStats: MutableMap<String, Int>) {
-    val changedFiles = commits.lines().filter { it.isNotBlank() }
+fun parseGitlogOutput(gitLogOutput: String): List<GitCommit> {
+    val lines = gitLogOutput.lines()
+    val indexes = lines.indices
+        .filter { lines[it].startsWith("commit") } + listOf(lines.size)
+
+    val commits = indexes.zipWithNext().map { (start, end) ->
+        GitCommit(lines.slice(start until end).joinToString(separator = "\n"))
+    }
+    return commits
+}
+
+suspend fun parseCommitFiles(commits: GitCommit, fileStats: MutableMap<String, Int>) {
+    val changedFiles = commits.files
     changedFiles.forEach { fileName ->
         fileStats[fileName] = fileStats.getOrPut(fileName) { 0 } + 1
     }
 }
 
+
+data class GitCommit(val commit: String, val branches: String, val author: String, val date: String, val message: String, val files: List<String>)
+{
+    companion object {
+        operator fun invoke(text: String): GitCommit {
+            val lines = text.lines()
+            val first = lines.first().removePrefix("commit ")
+            val commit = first.substringBefore(" ")
+            val branches = first.substringAfter(" ").removePrefix("(").removeSuffix(")")
+            val author = lines.first { it.startsWith("Author") }.removePrefix("Author:").trim()
+            val date = lines.first { it.startsWith("Date") }.removePrefix("Date:").trim()
+            val message = lines.filter { it.startsWith("    ") }.map { it.trim() }.joinToString(separator = "\n")
+            val startIndex = lines.indexOfLast { it.startsWith("    ") } + 1
+            val files = lines.subList(startIndex, lines.size)
+                .filter { it.isNotBlank() }
+            return GitCommit(commit, branches, author, date, message,files)
+        }
+    }
+
+}
--- src/commonTest/kotlin/cli/CliCommandTest.kt
+++ src/commonTest/kotlin/cli/CliCommandTest.kt
@@ -3,69 +3,46 @@ package cli
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-/***
- * CUSTOMIZE_ME: the tests are specific to git-standup and can be deleted once understood
- * Write your own unit tests!
- */
+
 class CliCommandTest {
-    private fun assertLogCommand(args: String, expected: String) {
-        fun String.simplify() =
-            replace("\\s+".toRegex(), " ").replace("'", "").replace("\"", "")
+    val expected = GitCommit(
+        commit = "db1ab6e5a5748a8eca2665a21d0e2d721a32ec8d",
+        branches = "HEAD -> main, origin/main, origin/HEAD",
+        author = "Jean-Michel Fayard <jmfayard@gmail.com>",
+        date = "Tue Jun 1 19:49:02 2021 +0200",
+        message = "Handle CUSTOMIZE_ME comments",
+        files = listOf("README.md", "bin/git-standup", "bin/git-stats", "installer.sh", "settings.gradle.kts")
+    )
 
-        val cmd = CliCommand()
-        cmd.main(args.split(" ").filter { it.isNotBlank() }.also { println(it) })
-        val actual = cmd.gitLogCommand().joinToString(separator = " ").simplify()
-        assertEquals(expected.simplify(), actual, "Invalid git log for args='$args'")
-    }
-
-
-    @Test
-    fun gitLogWithNoArgument() {
-        assertLogCommand(
-            "",
-            "git --no-pager log --all --no-merges --since=yesterday --author=me --abbrev-commit --oneline --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --date='relative' --color=always",
+    @Test fun parseACommit() {
+        assertEquals(
+            expected,
+            GitCommit(commit)
         )
     }
 
-    @Test
-    fun gitLogForExample() {
-        val expected =
-            "git --no-pager log --all --no-merges --since=\"yesterday\" --author=\"John\" --abbrev-commit --oneline --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --date='relative' --color=always"
-        assertLogCommand("-a John -w MON-FRI -m 3", expected)
-        assertLogCommand("--author John --week-day MON-FRI --depth 3", expected)
-    }
-
-    @Test
-    fun gitLogWithLotsOfParameters() {
-        val expected =
-            "git --no-pager log --first-parent main --no-merges --since=\"7 days ago\" --until=\"4 days ago\" --after=\"2021-01-01\" --author=\"John\" --abbrev-commit --oneline --pretty=format:'%Cred%h%Creset - %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date='short' --color=always"
-        assertLogCommand("-a John -b main -d 7 -u 4 -D short -A 2021-01-01 -B 2021-01-02 -R", expected)
-        assertLogCommand(
-            "--author John --branch main --days 7 --until 4 --date-format short --after 2021-01-01 --before 2021-01-02 --author-date",
-            expected
+    @Test fun parseThreeCommits() {
+        val gitLogOutput = "$commit\n$commit\n$commit"
+        val actual = parseGitlogOutput(gitLogOutput)
+        assertEquals(
+            expected = listOf(expected, expected, expected),
+            actual = actual
         )
     }
-
-    @Test
-    fun gitLogForGpg() {
-        val expected =
-            "git --no-pager log --all --no-merges --since=\"yesterday\" --author=\"John\" --abbrev-commit --oneline --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cd) %C(bold blue)<%an>%Creset %C(yellow)gpg: %G?%Creset' --date='relative' --color=always"
-        assertLogCommand("-g -a John", expected)
-        assertLogCommand("--gpg-signed --author John", expected)
-    }
-
-    @Test
-    fun findWithNoArguments() {
-        val cmd = CliCommand()
-        cmd.main(emptyList())
-        assertEquals("find . -maxdepth 2 -mindepth 0 -name .git".split(" "), cmd.findCommand())
-    }
-
-    @Test
-    fun findWithArguments() {
-        val cmd = CliCommand()
-        cmd.main(listOf("-L", "-m", "2"))
-        assertEquals("find . -L -maxdepth 3 -mindepth 0 -name .git".split(" "), cmd.findCommand())
-    }
-
 }
+
+val commit = """
+        commit db1ab6e5a5748a8eca2665a21d0e2d721a32ec8d (HEAD -> main, origin/main, origin/HEAD)
+        Author: Jean-Michel Fayard <jmfayard@gmail.com>
+        Date:   Tue Jun 1 19:49:02 2021 +0200
+
+            Handle CUSTOMIZE_ME comments
+
+        README.md
+        bin/git-standup
+        bin/git-stats
+        installer.sh
+        settings.gradle.kts
+    """.trimIndent()
+
+
